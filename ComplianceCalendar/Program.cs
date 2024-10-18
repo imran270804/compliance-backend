69f509bf92d5ba79b253a62db989ae21c6c054a5
Imrankhan A M <imrankhan270804@gmail.com>
2024-10-18 16:47:52 +0530

    Update Program.cs

--- ComplianceCalendar/Program.cs
+++ ComplianceCalendar/Program.cs
@@ -131,7 +131,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors("AllowLocalhost");
+    app.UseCors("SpecificOrigin");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
